0ead1882191e3432431cead115e9394e4a326f31
Marco Todaro <marcoposta59@gmail.com>
2020-07-26 11:31:06 +0200

    Updates

---
+++ precache-manifest.2c0ef429531bb8e94c20b7f58413a83f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16fbd0107ddfa0ff81cdd2e302dd793b",
+    "url": "/landingPageInfluencerPlatform/index.html"
+  },
+  {
+    "revision": "58388933d05144407256",
+    "url": "/landingPageInfluencerPlatform/static/css/main.2a122d11.chunk.css"
+  },
+  {
+    "revision": "8e2a0d4f4878c0b794d4",
+    "url": "/landingPageInfluencerPlatform/static/js/2.5593b48c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/landingPageInfluencerPlatform/static/js/2.5593b48c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58388933d05144407256",
+    "url": "/landingPageInfluencerPlatform/static/js/main.7009d8e4.chunk.js"
+  },
+  {
+    "revision": "729ae348bb0fd36fdcaf",
+    "url": "/landingPageInfluencerPlatform/static/js/runtime-main.dfd18dce.js"
+  },
+  {
+    "revision": "20cbd480edd75dd476f3d16c6a763f28",
+    "url": "/landingPageInfluencerPlatform/static/media/bg-masthead.20cbd480.jpg"
+  },
+  {
+    "revision": "51737fa52c20bb0e07b10d8c2df644ed",
+    "url": "/landingPageInfluencerPlatform/static/media/sfondo.51737fa5.jpg"
+  }
+]);
