d3f2f6a037d03a05f8ef73eb3c2388a908160bd8
Marco Todaro <marcoposta59@gmail.com>
2020-07-26 09:18:42 +0200

    aggiornati i dati degli influencer!

--- src/data/influencer.js
+++ src/data/influencer.js
@@ -9,40 +9,53 @@ const influencer = [
     "MI",
     ["Travel"],
     "https://cdn.pixabay.com/photo/2020/07/22/12/34/portrait-5428894_960_720.jpg",
-    ["ig", "yt", "fb"]
+    [
+      { nome: "ig", flw: 1000, eng: 200 },
+      { nome: "yt", flw: 2000, eng: 100 },
+      { nome: "fb", flw: 5000, eng: 400 },
+    ]
   ),
   new Model(
     "02",
     "Giulia",
     "Verdi",
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas nec turpis vitae ipsum hendrerit facilisis ultrices et leo. Nullam viverra venenatis risus, eu commodo leo condimentum eu. ",
-
     "RE",
     ["Vlog"],
     "https://cdn.pixabay.com/photo/2020/07/21/10/52/girl-5425872_960_720.jpg",
-    ["ig", "yt", "fb"]
+    [
+      { nome: "ig", flw: 1000, eng: 200 },
+      { nome: "yt", flw: 2000, eng: 100 },
+      { nome: "fb", flw: 5000, eng: 400 },
+    ]
   ),
   new Model(
     "03",
     "Maria",
     "Gialli",
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas nec turpis vitae ipsum hendrerit facilisis ultrices et leo. Nullam viverra venenatis risus, eu commodo leo condimentum eu. ",
-
     "MI",
     ["Food"],
     "https://cdn.pixabay.com/photo/2020/07/21/04/19/woman-5425033_960_720.jpg",
-    ["ig", "yt", "fb"]
+    [
+      { nome: "ig", flw: 1000, eng: 200 },
+      { nome: "yt", flw: 2000, eng: 100 },
+      { nome: "fb", flw: 5000, eng: 400 },
+    ]
   ),
   new Model(
     "04",
     "Marta",
     "Giorgi",
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas nec turpis vitae ipsum hendrerit facilisis ultrices et leo. Nullam viverra venenatis risus, eu commodo leo condimentum eu. ",
-
     "RE",
     ["Fitness"],
     "https://cdn.pixabay.com/photo/2020/07/14/06/41/yoga-5403001_960_720.jpg",
-    ["ig", "yt", "fb"]
+    [
+      { nome: "ig", flw: 1000, eng: 200 },
+      { nome: "yt", flw: 2000, eng: 100 },
+      { nome: "fb", flw: 5000, eng: 400 },
+    ]
   ),
   new Model(
     "05",
@@ -52,7 +65,11 @@ const influencer = [
     "MI",
     ["Business", "Fitness"],
     "https://cdn.pixabay.com/photo/2020/07/19/14/53/man-5420287_960_720.jpg",
-    ["ig", "yt", "fb"]
+    [
+      { nome: "ig", flw: 1000, eng: 200 },
+      { nome: "yt", flw: 2000, eng: 100 },
+      { nome: "fb", flw: 5000, eng: 400 },
+    ]
   ),
 ];
 
