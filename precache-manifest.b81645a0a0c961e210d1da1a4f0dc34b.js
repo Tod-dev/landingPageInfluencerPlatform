f1655de196881fe442cc3423218aba1b1e0b13ba
Marco Todaro <marcoposta59@gmail.com>
2020-07-26 12:13:43 +0200

    Updates

---
+++ precache-manifest.b81645a0a0c961e210d1da1a4f0dc34b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fddd67ca4beb1edbf36396d191c1c8f0",
+    "url": "/landingPageInfluencerPlatform/index.html"
+  },
+  {
+    "revision": "7d745f4c8b388c805f97",
+    "url": "/landingPageInfluencerPlatform/static/css/main.2a122d11.chunk.css"
+  },
+  {
+    "revision": "8e2a0d4f4878c0b794d4",
+    "url": "/landingPageInfluencerPlatform/static/js/2.5593b48c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/landingPageInfluencerPlatform/static/js/2.5593b48c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d745f4c8b388c805f97",
+    "url": "/landingPageInfluencerPlatform/static/js/main.8fe977be.chunk.js"
+  },
+  {
+    "revision": "729ae348bb0fd36fdcaf",
+    "url": "/landingPageInfluencerPlatform/static/js/runtime-main.dfd18dce.js"
+  },
+  {
+    "revision": "20cbd480edd75dd476f3d16c6a763f28",
+    "url": "/landingPageInfluencerPlatform/static/media/bg-masthead.20cbd480.jpg"
+  },
+  {
+    "revision": "51737fa52c20bb0e07b10d8c2df644ed",
+    "url": "/landingPageInfluencerPlatform/static/media/sfondo.51737fa5.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1e5747f47c463baa1cd858e99df49f68.js"
+  "/landingPageInfluencerPlatform/precache-manifest.b81645a0a0c961e210d1da1a4f0dc34b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/landingPageInfluencerPlatform/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
