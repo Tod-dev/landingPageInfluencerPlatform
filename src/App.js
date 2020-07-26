163e6861a7131a50fdc850e315f7ba511bafe84e
Marco Todaro <marcoposta59@gmail.com>
2020-07-26 12:10:54 +0200

    rrdom problem solved ?

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ const App = () => {
   return (
     <Provider store={store}>
       <Container fluid="true">
-        <Router>
+        <Router basename={process.env.NODE_ENV}>
           <Switch>
             <Route exact path="/" component={Home} />
             <Route exact path="/home" component={Home} />
